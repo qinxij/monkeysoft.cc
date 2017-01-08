ebfb13930e0c793cceef706b29ae97196696f3f8
qinxij <qinxij@163.com>
2017-01-09 04:38:16 +0800

    refact: remove no need dependencies

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
